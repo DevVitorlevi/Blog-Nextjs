692bbe13743ad0977539dd4c5947c383faddcf09
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-08-14 15:28:21 -0300

    feat:PostModel criado

---
+++ blog/src/models/post/post-model.ts
@@ -0,0 +1,14 @@
+
+
+export type PostModel = {
+    id: string
+    title: string
+    slug: string
+    excerpt: string
+    content: string
+    coverImageUr: string
+    published: boolean
+    createdAt: string
+    updatedAt: string
+    author: string
+}
